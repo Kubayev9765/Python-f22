76a59bf1acb5122c3d6240b7b3f541607e1f236f
Kubayev9765 <ShamanRamilov@gmail.com>
2025-05-24 19:43:29 +0500

    Create homework.py

---
+++ lesson-1/homework/homework.py
@@ -0,0 +1,46 @@
+1.
+#Kvadratni parametrini o'lchash
+a=int(input('Kvadratning tomonini kiriting:' ))
+#Kvadratning Parametrini topish uchun 4 ga kupaytiramiz
+P=4*a
+print('Parametri', P)
+
+#Kvadratni yuzini topish
+a=int(input('Kvadratning tomonini kiriting:' ))
+#Kvadratning Yuzini topish 
+S=a**2
+print('Kvadratnuning Yuzi: ',S)
+
+
+2.
+p=3.14
+d=int(input('Aylananing diametrini kiriting: '))
+#Aylananing uzunligini topish
+C=d*p
+print('Aylananig uzunligi :',C)
+
+3.
+# Ikkita butun son berilgan
+a=int(input('Birinchi sonni kiriting: '))
+b=int(input('Ikkinchi sonni kiriting: '))
+# Ikkita sonni o'rtacha qiymatini topish
+c=(a+b)/2
+print("Ikkita sonni O'rta qiymati :", c)
+
+4.
+import math
+# Ikkita butun son berilgan
+a=int(input('Birinchi sonni kiriting: '))
+b=int(input('Ikkinchi sonni kiriting: '))
+#Ikkita sonni yig'indisi
+c=a+b
+#Ko'paytmasi
+d=a*b
+#Ikila sonni kvadrat ildizi
+e=math.sqrt(a)
+f=math.sqrt(b)
+print("Ikki sonni yig'indisi: ", c)
+print("Ikki sonni ko'paytmasi: ", d)
+print("Birinchi sonni kvadrat ildizi: ",e)
+print("Ikkinchi sonni kvadrat ildizi: ",f)
+
